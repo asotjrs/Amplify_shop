0f5af415f70fcdcede10dbf01423e2d184fdf6e8
Djafer Abdelhadi <41378212+asotjrs@users.noreply.github.com>
2021-08-28 00:51:01 +0200

    add auth

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
+  },
+});
+
 AppRegistry.registerComponent(appName, () => App);
